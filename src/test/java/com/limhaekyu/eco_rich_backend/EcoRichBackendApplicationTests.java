520b5e4a64b428499043a55bc45af5fade3a3395
limhaekyu <limk9500@gmail.com>
2023-04-12 00:04:38 +0900

    Initial commit

---
+++ src/test/java/com/limhaekyu/eco_rich_backend/EcoRichBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limhaekyu.eco_rich_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoRichBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
